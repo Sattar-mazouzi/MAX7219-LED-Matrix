2a7ff49025dbbcff50ce117894dd446df95d1d32
Abdesattar Mazouzi <36484732+Sattar-mazouzi@users.noreply.github.com>
2026-03-21 22:28:16 +0100

    Create the initialization function, write function
    Create the initialize function, and the write function to write values to the custom registers of the MAX1792 IC through SPI connection

---
+++ include/max7219.h
@@ -0,0 +1,44 @@
+#ifndef MAX7219_H
+#define MAX7219_H
+
+/* --- MAX7219 Register Addresses --- */
+#define MAX7219_REG_NOOP         0x00
+#define MAX7219_REG_DIGIT0       0x01 // Row 1
+#define MAX7219_REG_DIGIT1       0x02 // Row 2
+#define MAX7219_REG_DIGIT2       0x03 // Row 3
+#define MAX7219_REG_DIGIT3       0x04 // Row 4
+#define MAX7219_REG_DIGIT4       0x05 // Row 5
+#define MAX7219_REG_DIGIT5       0x06 // Row 6
+#define MAX7219_REG_DIGIT6       0x07 // Row 7
+#define MAX7219_REG_DIGIT7       0x08 // Row 8
+#define MAX7219_REG_DECODE_MODE  0x09
+#define MAX7219_REG_INTENSITY    0x0A
+#define MAX7219_REG_SCAN_LIMIT   0x0B
+#define MAX7219_REG_SHUTDOWN     0x0C
+#define MAX7219_REG_DISPLAY_TEST 0x0F
+
+/* --- MAX7219 Default Data Values --- */
+#define MAX7219_MODE_DECODE_OFF  0x00 // Use for 8x8 Matrix (Raw Pixels)
+#define MAX7219_MODE_DECODE_ON   0xFF // Use for 7-Segment displays
+#define MAX7219_SCAN_ALL         0x07 // Scan all 8 rows
+#define MAX7219_SHUTDOWN_OFF     0x01 // Normal operation (Power On)
+#define MAX7219_SHUTDOWN_ON      0x00 // Power-saving mode (Power Off)
+#define MAX7219_TEST_ON          0x01 // All LEDs On
+#define MAX7219_TEST_OFF         0x00 // Normal operation
+
+/*typedef strcutcs*/
+typedef struct 
+{
+    uint8_t DECODE_MODE_config; 
+    uint8_t INTENSITY_config; 
+    uint8_t SCAN_LIMIT_config; 
+    uint8_t SHUTDOWN_config; 
+    // uint8_t DISPLAY_TEST_config; 
+}max7219_config;
+
+
+/* Function declartion */
+void max7219_write(uint8_t reg, uint8_t data, spi_device_handle_t spi); 
+void max7219_init(max7219_config * config, spi_device_handle_t spi ); 
+
+#endif
---
+++ max7219.c
@@ -0,0 +1,32 @@
+#include "driver/spi_master.h"
+#include<string.h>
+#include"max7219.h" 
+
+// Function to send a command to MAX7219
+void max7219_write(uint8_t reg, uint8_t data, spi_device_handle_t spi) {
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));
+    uint8_t tx_data[2] = {reg, data};
+    t.length = 16;
+    t.tx_buffer = tx_data;
+    spi_device_polling_transmit(spi, &t);
+}
+
+void max7219_init(max7219_config *config,spi_device_handle_t spi) {
+    // 1. Wake up the chip using the value stored in the config struct
+    max7219_write(MAX7219_REG_SHUTDOWN, config->SHUTDOWN_config, spi);
+    
+    // 2. Set Decode Mode
+    max7219_write(MAX7219_REG_DECODE_MODE, config->DECODE_MODE_config, spi);
+    
+    // 3. Set Scan Limit
+    max7219_write(MAX7219_REG_SCAN_LIMIT, config->SCAN_LIMIT_config, spi);
+    
+    // 4. Set Intensity
+    max7219_write(MAX7219_REG_INTENSITY, config->INTENSITY_config,spi); 
+    
+    // Clear the display
+    for (int i = 1; i <= 8; i++) {
+        max7219_write(i, 0x00, spi);
+    }
+}
